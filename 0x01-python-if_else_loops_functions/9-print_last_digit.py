03737099ca2f189d5afc7f5d73dba070352c89ef
Saysherief <suhairdama@gmail.com>
2023-05-09 19:42:28 -0700

    Prints and returns last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    """
+    Returns value of the last digit
+    """
+    last_dig = abs(number) % 10
+    print({}.format(last_dig), end="")
+    return last_dig
