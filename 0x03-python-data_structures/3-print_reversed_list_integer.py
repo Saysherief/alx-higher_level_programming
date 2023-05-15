243b5676f294e7168b1548cf21c8aa4922d3be9f
Saysherief <suhairdama@gmail.com>
2023-05-15 06:16:26 -0700

    If empty list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,5 +4,6 @@ def print_reversed_list_integer(my_list=[]):
     A function that prints all integers of a list in reverse
     """
     num = len(my_list) - 1
-    for i in range(num, -1, -1):
-        print("{:d}".format(my_list[i]))
+    if num >= 0:
+        for i in range(num, -1, -1):
+            print("{:d}".format(my_list[i]))
