890c2ea030a17ab8d8b03b9f8eb7d6c03b80fe9d
Saysherief <suhairdama@gmail.com>
2023-05-15 07:41:14 -0700

    managed space

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,11 @@ def print_matrix_integer(matrix=[[]]):
     A function that prints a matrix of integers
     """
     for row in matrix:
+        i = 0
         for num in row:
-            print("{:d} ".format(num), end="")
+            print("{:d}".format(num), end="")
+            if i == len(row) - 1:
+                break
+            print(end=" ")
+            i += 1
         print()
