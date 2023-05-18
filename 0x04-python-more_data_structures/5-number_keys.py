809fb61034790ddbade37eabfcc4a02b17b9294d
Saysherief <suhairdama@gmail.com>
2023-05-17 19:27:02 -0700

    No. of keys

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """
+    A function that returns the number of keys in a dictionary
+    """
+    keys = 0
+    for key in a_dictionary:
+        keys += 1
+    return keys
