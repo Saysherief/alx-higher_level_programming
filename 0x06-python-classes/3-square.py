10a86a5394956347700fe422df41f0921a7168c7
Saysherief <suhairdama@gmail.com>
2023-05-31 15:29:09 -0700

    Added public instance method area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """
+    A class Square that defines a square
+
+    Attributes:
+        __size: The size of the square
+    """
+    def __init__(self, new_size=0):
+        """
+        Initializes a new Square object
+
+        Args:
+            new_size: The size of the new square, by default 0
+
+        Raises:
+            TypeError: if args is not an integer
+            ValueError: if args is less than 0
+        """
+        if not isinstance(new_size, int):
+            raise TypeError("size must be an integer")
+        elif new_size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = new_size
+
+    def area(self):
+        """
+        Returns area of the square
+        """
+        return self.__size ** 2
