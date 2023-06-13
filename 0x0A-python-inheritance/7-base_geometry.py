a34b8470750f9e2b484894e3682cb8ab686f2df5
Saysherief <suhairdama@gmail.com>
2023-06-12 17:23:18 -0700

    Another method that validates value

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """
+    A class BaseGeometry that defines a Geometry
+    """
+    def area(self):
+        """
+        A function that returns the area of the Base Geometry
+        Raises:
+            Exception that it is not implemented yet
+        """
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """
+        A function that validates value
+        Raises:
+            TypeError and ValueError for non integer and negative
+            integer respectively
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
