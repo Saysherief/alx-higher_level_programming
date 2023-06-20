0979ce87210c86fc73946e86717b04b06bb60ab0
Saysherief <suhairdama@gmail.com>
2023-06-20 03:08:44 -0700

    update #0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -138,6 +138,26 @@ class Rectangle(Base):
         else:
             self.__y = value
 
+    def update(self, *args):
+        '''
+        A public method that assigns an argument to each attribute:
+        1st: id
+        2nd: width
+        3rd: height
+        4th: x
+        5th: y
+        '''
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
+
     def display(self):
         '''
         Prints the rectangle according to the given dimention
