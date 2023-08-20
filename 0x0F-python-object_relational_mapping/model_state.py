23349660072526e81286721f7e0c4f81bfb23b4e
Saysherief <suhairdama@gmail.com>
2023-08-20 14:17:21 -0700

    Create Engine for my 1st model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,11 +6,21 @@ Base = declarative_base()
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
 
 Base = declarative_base()
 
 
 class State(Base):
+    """ Class State that inherits from Base
+    Represens ORM for state
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+
+
+# Create the engine to connect to the MySQL server
+engine = create_engine(
+        'mysql+mysqldb://root:root@localhost:3306/hbtn_0e_6_usa'
+        )
