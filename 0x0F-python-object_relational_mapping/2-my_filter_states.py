a2f1caf57160b9c5e5b925e362df97dbaa218dde
Saysherief <suhairdama@gmail.com>
2023-08-20 02:40:04 -0700

    Takes in an argument and displays all values in the states table of given database where name matches the argument

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -27,8 +27,8 @@ if __name__ == '__main__':
     cursor = conn.cursor()
 
     ''' Execute the query to fetch all states '''
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY
-    id ASC".format(state)
+    query = "SELECT * FROM states WHERE name = "
+    query += "'{}' ORDER BY id ASC".format(state)
     cursor.execute(query)
 
     ''' Fetch all rows then display all states in new line'''
