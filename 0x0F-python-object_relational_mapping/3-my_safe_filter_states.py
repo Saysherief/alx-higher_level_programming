2b5662aab20b2cc9ac69d274e2b60b583e0553a1
Saysherief <suhairdama@gmail.com>
2023-08-20 02:53:36 -0700

    Safe from SQL injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+That takes in an argument and displays all values in the states table of
+the given database where name matches the argument
+Safe from SQL injection
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    ''' Get MySQL commands from Command-line arguments '''
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state = sys.argv[4]
+
+    ''' Connect to MySQL server '''
+    conn = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+            )
+
+    ''' Create a cursor object to execute quries '''
+    cursor = conn.cursor()
+
+    ''' Execute the query to fetch all states '''
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (state,))
+
+    ''' Fetch all rows then display all states in new line'''
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    ''' close the cursor and db connection '''
+    cursor.close()
+    conn.close()
