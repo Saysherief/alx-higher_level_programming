fece45c82cad57be944f029000f2c0ae4afd293d
Saysherief <suhairdama@gmail.com>
2023-08-20 02:02:42 -0700

    lists all states with a name starting with N (upper N) from the database

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+That lists all states from the database hbtn_0e_0_usa
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    ''' Get MySQL commands from Command-line arguments '''
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    ''' Connect to MySQL server '''
+    conn = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+            )
+
+    ''' Create a cursor object to execute quries '''
+    cursor = conn.cursor()
+
+    ''' Execute the query to fetch all states '''
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    ''' Fetch all rows then display all states row by row'''
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    ''' close the cursor and db connection '''
+    cursor.close()
+    conn.close()
