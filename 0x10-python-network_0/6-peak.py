c82a5ba6191a6a1f9ebcedb399185e0f6b7abcb5
Saysherief <suhairdama@gmail.com>
2023-08-31 16:31:59 -0700

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,4 +6,14 @@ def find_peak(list_of_integers):
     """Returns the peak"""
     if list_of_integers == []:
         return None
-    return max(list_of_integers)
+    low = 0
+    high  = len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+        if list_of_integers[mid] < list_of_integers[mid +1]:
+            low = mid + 1
+        else:
+            high = mid
+    return list_of_integers[low]
