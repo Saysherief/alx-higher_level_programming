7a3a1e3c28cbaf761250ee6dc70c028e5e1cf3e4
Saysherief <suhairdama@gmail.com>
2023-08-31 16:35:34 -0700

    Space

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,12 +7,12 @@ def find_peak(list_of_integers):
     if list_of_integers == []:
         return None
     low = 0
-    high  = len(list_of_integers) - 1
+    high = len(list_of_integers) - 1
 
     while low < high:
         mid = (low + high) // 2
 
-        if list_of_integers[mid] < list_of_integers[mid +1]:
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
             low = mid + 1
         else:
             high = mid
