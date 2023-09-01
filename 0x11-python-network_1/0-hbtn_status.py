93fdbaef5d23c5c83e80b6b0b79611bb906c3a4b
Saysherief <suhairdama@gmail.com>
2023-09-01 15:58:23 -0700

    code should not be executed when imported

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,12 +2,13 @@
 """A script that fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
-url = "https://alx-intranet.hbtn.io/status"
-with urllib.request.urlopen(url) as response:
-    body = response.read()
-    utf8_cont = body.decode('utf-8')
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        utf8_cont = body.decode('utf-8')
 
-print("Body response:")
-print("\t- type: {}".format(type(body)))
-print("\t- content: {}".format(body))
-print("\t- utf8 content: {}".format(utf8_cont))
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(utf8_cont))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,8 +4,8 @@ value of the X-Request-Id variable found in the header of the response"""
 import urllib.request
 import sys
 
-url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
-    header_id = response.headers.get("X-Request-Id")
-
-print(header_id)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        header_id = response.headers.get("X-Request-Id")
+    print(header_id)
